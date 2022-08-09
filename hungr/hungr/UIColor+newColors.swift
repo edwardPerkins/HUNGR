06d1762c315cbef4ff09c5769ac848c77f205461
maherDamouni <damounim1.gmail.com>
2022-08-09 12:16:31 -0400

    extended UIColor class to have 4 colors

---
+++ hungr/hungr/UIColor+newColors.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+newColors.swift
+//  hungr
+//
+//  Created by Maher Damouni on 8/9/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var Background = UIColor(named: "Background")
+    static var TextColor = UIColor(named: "TextColor")
+    static var AccentOne = UIColor(named: "AccentOne")
+    static var AccentTwo = UIColor(named: "AccentTwo")
+}
