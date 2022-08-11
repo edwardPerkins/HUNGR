8b274b93d960182d3540de0d14981f3a8e24551e
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-08-11 12:39:07 -0400

    started collectionView cell

---
+++ hungr/hungr/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  hungr
+//
+//  Created by Spencer Hurd on 8/11/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    var nameLabelCV: UIView
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
