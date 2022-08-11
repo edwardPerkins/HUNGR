e928404232cd925ad29c70806028b8f2072eafea
Joel Maldonado <joelmaldonado@Joels-MacBook-Air.local>
2022-08-11 14:16:54 -0400

    MyMeals Storyboard created

---
+++ hungr/hungr/MyMealCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyMealCell.swift
+//  hungr
+//
+//  Created by Joel Maldonado on 8/11/22.
+//
+
+import UIKit
+
+class MyMealCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
